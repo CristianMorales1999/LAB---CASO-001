7a9facae45373e65461b808cee1c3776877c987f
CristianMorales1999 <cm9225064@gmail.com>
2025-05-18 06:20:29 -0500

    Correción de rotacion de flechas en vista mobil

--- js/script.js
+++ js/script.js
@@ -3,39 +3,57 @@
     const list = document.querySelector('.menu__links');
     const menu = document.querySelector('.menu__hamburguer');
 
-    const addClick = ()=>{
-        listElements.forEach(element =>{
-            element.addEventListener('click', ()=>{
+    const addClick = () => {
+        listElements.forEach(element => {
+            element.addEventListener('click', () => {
                 let subMenu = element.children[1];
+                let arrow = element.querySelector('.menu__arrow');
                 let height = 0;
+                
                 element.classList.toggle('menu__item--active');
-                if(subMenu.clientHeight === 0){
+                
+                if (subMenu.clientHeight === 0) {
                     height = subMenu.scrollHeight;
+                    // Ajusta la flecha cuando el submenu está abierto
+                    arrow.style.transform = 'rotate(0deg)';
+                } else {
+                    // Vuelve la flecha a la posición inicial (derecha)
+                    arrow.style.transform = 'rotate(-90deg)';
                 }
+
                 subMenu.style.height = `${height}px`;
             });
         });
     }
-    const deleteStyleHeight = ()=>{
-        listElements.forEach(element=>{
-            if(element.children[1].getAttribute('style')){
+
+    const deleteStyleHeight = () => {
+        listElements.forEach(element => {
+            if (element.children[1].getAttribute('style')) {
                 element.children[1].removeAttribute('style');
                 element.classList.remove('menu__item--active');
+                
+                // Restablece la rotación de las flechas
+                const arrow = element.querySelector('.menu__arrow');
+                if (arrow) {
+                    arrow.style.transform = 'rotate(-90deg)';
+                }
             }
         });
     }
-    window.addEventListener('resize', ()=>{
-        if(window.innerWidth > 800){
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 800) {
             deleteStyleHeight();
-            if(list.classList.contains('menu__links--show'))
+            if (list.classList.contains('menu__links--show'))
                 list.classList.remove('menu__links--show');
-        }else{
+        } else {
             addClick();
         }
     });
-    if(window.innerWidth <= 800){
+
+    if (window.innerWidth <= 800) {
         addClick();
     }
-    menu.addEventListener('click', ()=> list.classList.toggle('menu__links--show'));
-})();
 
+    menu.addEventListener('click', () => list.classList.toggle('menu__links--show'));
+})();
